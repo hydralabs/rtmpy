797a5fb8f1a1ac342cbc90809fb27605e3708c54
Thijs Triemstra <thijs@collab.com>
2008-02-19 21:12:34 +0000

    Update for setup.py
    git-svn-id: https://svn.rtmpy.org/rtmpy/trunk@190 4c711123-bc32-0410-8b01-cccf66a6a824

--- setup.py
+++ setup.py
@@ -13,16 +13,32 @@ install_requires = ['Twisted>=2.5.0', 'PyAMF>=0.1.1']
 if sys.platform.startswith('win'):
     install_requires.append('PyWin32')
 
+long_desc = """\
+RTMPy is a Twisted protocol implementing Adobe's Real
+Time Messaging Protocol (RTMP), used for full-duplex
+real-time communication with applications running inside
+the Flash Player."""
+
+keyw = """\
+rtmp flv rtmps rtmpe amf amf0 amf3 flex flash http https
+streaming video audio sharedobject webcam record playback
+flashplayer air actionscript decoder encoder gateway"""
+
 setup(name = "RTMPy",
     version = "0.1",
     description = "Twisted protocol for RTMP",
+    long_description = long_desc,
     url = "http://rtmpy.org",
+    author = "The RTMPy Project",
+    author_email = "rtmpy-dev@rtmpy.org",
+    keywords = keyw,
     packages = ["rtmpy"],
     test_suite = "rtmpy.tests.suite",
     install_requires = install_requires,
     license = "MIT License",
     classifiers = [
         "Development Status :: 2 - Pre-Alpha",
+        "Natural Language :: English",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
