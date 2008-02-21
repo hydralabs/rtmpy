9a079bd88fce2e0e9a6ea5e0306f3d8e4b7378b8
Nick Joyce <nick@boxdesign.co.uk>
2008-02-21 09:44:08 +0000

    Added darwin uptime function and more tests
    git-svn-id: https://svn.rtmpy.org/rtmpy/trunk@201 4c711123-bc32-0410-8b01-cccf66a6a824

--- rtmpy/tests/test_util.py
+++ rtmpy/tests/test_util.py
@@ -5,16 +5,58 @@
 Tests for L{rtmpy.util}
 """
 
+import sys, time, warnings
+
 from twisted.internet import reactor, defer
 from twisted.trial import unittest
 
 from rtmpy import util
 
+
 class UptimeTestCase(unittest.TestCase):
     def test_uptime(self):
         util.uptime()
+        util.boottime = None
 
-import sys
+    if sys.platform.startswith('linux'):
+        def test_linux(self):
+            now = time.time()
+            ut = util.uptime_linux()
 
-if sys.platform.startswith('darwin'):
-    UptimeTestCase.test_uptime.todo = unittest.makeTodo((NotImplementedError, ''))
+            self.assertTrue(type(ut), float)
+            self.assertTrue(now > ut)
+
+    elif sys.platform.startswith('win'):
+        def test_win32(self):
+            now = time.time()
+            ut = util.uptime_win32()
+
+            self.assertTrue(type(ut), float)
+            self.assertTrue(now > ut)
+
+    elif sys.platform.startswith('darwin'):
+        def test_darwin(self):
+            now = time.time()
+            ut = util.uptime_darwin()
+
+            self.assertTrue(type(ut), float)
+            self.assertTrue(now > ut)
+
+
+class UnknownPlatformUptimeTestCase(unittest.TestCase):
+    def setUp(self):
+        self.platform = sys.platform
+        sys.platform = ''
+        util.boottime = None
+
+    def tearDown(self):
+        sys.platform = self.platform
+
+    def test_warning(self):
+        warnings.filterwarnings('error', category=RuntimeWarning)
+        self.assertRaises(RuntimeWarning, util.uptime)
+
+        warnings.filterwarnings('ignore', category=RuntimeWarning)
+        self.assertEquals(util.uptime(), 0)
+
+        self.assertTrue(abs(util.boottime - (time.time() * 1000)) < 0.5)
--- rtmpy/util.py
+++ rtmpy/util.py
@@ -10,7 +10,10 @@ RTMPy Utilities.
 @since: 0.1
 """
 
-import sys
+import os, sys, time
+
+# the number of milliseconds since the epoch
+boottime = None
 
 try:
     from cStringIO import StringIO
@@ -20,11 +23,21 @@ except ImportError:
 from pyamf.util import BufferedByteStream
 
 def uptime_win32():
+    """
+    Returns the number of seconds since the epoch.
+
+    @rtype: C{float}
+    """ 
     import win32api
 
-    return win32api.GetTickCount()
+    return float(time.time()) - float(win32api.GetTickCount() / 1000)
 
 def uptime_linux():
+    """
+    Returns the number of seconds since the epoch.
+
+    @rtype: C{float}
+    """ 
     try:
         fp = open('/proc/uptime')
     except IOError:
@@ -33,16 +46,49 @@ def uptime_linux():
     buffer = fp.read()
     fp.close()
 
-    return int(float(buffer.split(" ")[0]) * 1000)
+    return float(time.time()) - float(buffer.split(" ")[0])
 
 def uptime_mac():
-    raise NotImplementedError
+    """
+    Returns the number of milliseconds since the kernel was started.
+    """ 
+    try:
+        fp = os.popen('sysctl -nb kern.boottime')
+    except IOError:
+        return 0
 
-if sys.platform.startswith('linux'):
-    uptime = uptime_linux
-elif sys.platform.startswith('win'):
-    uptime = uptime_win32
-elif sys.platform.startswith('darwin'):
-    uptime = uptime_mac()
-else:
-    uptime = lambda: 0
+    buffer = fp.read()
+    fp.close()
+
+    return float(time.time()) - float(buffer.strip())
+
+def uptime():
+    """
+    Returns the number of milliseconds since the system was booted.
+
+    @rtype: C{int}
+    """
+    global boottime
+
+    now = int(time.time() * 1000)
+
+    if boottime is not None:
+        return now - boottime
+
+    if sys.platform.startswith('linux'):
+        up_func = uptime_linux
+    elif sys.platform.startswith('win'):
+        up_func = uptime_win32
+    elif sys.platform.startswith('darwin'):
+        up_func = uptime_mac
+    else:
+        import warnings
+
+        warnings.warn("Could not find a platform specific uptime " \
+            "function for '%s'" % sys.platform, RuntimeWarning)
+
+        up_func = lambda: time.time()
+
+    boottime = int(up_func() * 1000)
+
+    return now - boottime
