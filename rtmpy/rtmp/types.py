d5eeda262e2bc340bdc258c55f87599005cc8eb2
Thijs Triemstra <thijs@collab.com>
2009-03-17 18:01:58 +0000

    Doc update from osflash rtmp spec
    git-svn-id: https://svn.rtmpy.org/rtmpy/trunk@376 4c711123-bc32-0410-8b01-cccf66a6a824

--- rtmpy/rtmp/types.py
+++ rtmpy/rtmp/types.py
@@ -5,19 +5,32 @@
 RTMP Channel type declarations.
 """
 
+#: Changes the chunk size for packets
 FRAME_SIZE = 0x01
 # 0x02 is unknown
+#: Send every x bytes read by both sides
 BYTES_READ = 0x03
+#: Ping is a stream control message, has subtypes
 PING = 0x04
+#: The servers downstream bandwidth
 SERVER_BANDWIDTH = 0x05
+#: The clients upstream bandwidth
 CLIENT_BANDWIDTH = 0x06
+#: Packet containing audio
 AUDIO_DATA = 0x07
+#: Packet containing video data
 VIDEO_DATA = 0x08
 # 0x0a - 0x0e is unknown
-FLEX_SHARED_OBJECT = 0x10 # ?
+#: Shared object with variable length
+FLEX_SHARED_OBJECT = 0x10
+#: Shared message with variable length
 FLEX_MESSAGE = 0x11
+#: An invoke which does not expect a reply
 NOTIFY = 0x12
+#: Has subtypes
 SHARED_OBJECT = 0x13
+#: Like remoting call, used for stream actions too
 INVOKE = 0x14
 # 0x15 anyone?
+#: FLV data
 FLV_DATA = 0x16
