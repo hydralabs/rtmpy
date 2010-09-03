618f8fdbb99bd8a5b2fde88ee1d303f6068ba4f2
Nick Joyce <nick@boxdesign.co.uk>
2010-09-03 12:21:15 +1000

    Doc update

--- rtmpy/protocol/rtmp/__init__.py
+++ rtmpy/protocol/rtmp/__init__.py
@@ -8,14 +8,13 @@ The Real Time Messaging Protocol (RTMP) is a protocol that is primarily used
 to stream audio and video over the internet to the U{Adobe Flash Player<http://
 en.wikipedia.org/wiki/Flash_Player>}.
 
-The protocol is a container for data packets which may be
-U{AMF<http://osflash.org/documentation/amf>} or raw audio/video data like
-found in U{FLV<http://osflash.org/flv>}. A single connection is capable of
-multiplexing many NetStreams using different channels. Within these channels
-packets are split up into fixed size body chunks.
+The protocol is a container for data packets which may be U{AMF<http://osflash
+.org/documentation/amf>} or raw audio/video data like found in U{FLV<http://
+osflash.org/flv>}. A single connection is capable of multiplexing many
+NetStreams using different channels. Within these channels packets are split up
+into fixed size body chunks.
 
-@see: U{RTMP (external)<http://rtmpy.org/wiki/RTMP>}
-@since: 0.1
+@see: U{RTMP<http://dev.rtmpy.org/wiki/RTMP>}
 """
 
 from twisted.internet import protocol, task
