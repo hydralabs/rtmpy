eb587ebc3d3db0c95bf033f1779212e5d4aaaaff
Thijs Triemstra <info@collab.nl>
2010-09-09 03:36:36 +0800

    Add clientDisconnected def.

--- doc/tutorials/examples/general/helloworld/python/server.py
+++ doc/tutorials/examples/general/helloworld/python/server.py
@@ -8,6 +8,10 @@ class LiveApplication(server.Application):
     Live app.
     """
 
+    def clientDisconnected(self, client, reason):
+        print 'client "%s" disconnected: %s' % (client, reason)
+
+
 app = LiveApplication()
 
 reactor.listenTCP(1935, server.ServerFactory({
