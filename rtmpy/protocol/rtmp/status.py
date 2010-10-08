b98dba0d26db74b56407e318bbfb28fe26f929f3
Nick Joyce <nick@boxdesign.co.uk>
2010-10-08 16:20:35 +1000

    Doc update for status

--- rtmpy/protocol/rtmp/status.py
+++ rtmpy/protocol/rtmp/status.py
@@ -1,8 +1,36 @@
+# Copyright the RTMPy project.
+
 """
+RTMP status response objects require guaranteed attribute order otherwise the
+Flash Player throws a fit.
 """
+
+
+def status(code, description=None, **kwargs):
+    """
+    A helper method to return a status object with prefilled defaults.
+
+    Use this if you want to return a success status for a given operation.
+    """
+    return Status('status', code, description, **kwargs)
+
+
+def error(code, description=None, **kwargs):
+    """
+    Use this if you want to return a failure status for a given operation.
+    """
+    return Status('error', code, description, **kwargs)
+
 
 class Status(object):
     """
+    Represents a status response for a given RTMP call or operation.
+
+    @ivar level: level of the status. Valid values appear to be 'status' or
+        'error'.
+    @ivar code: Represents the type of status/error being reported. See
+        L{rtmpy.statuscodes} for a list.
+    @ivar description: A description of the status.
     """
 
     class __amf__:
@@ -26,14 +54,3 @@ class Status(object):
     def __eq__(self, other):
         return self.__dict__ == other
 
-
-def status(code, description=None, **kwargs):
-    """
-    """
-    return Status('status', code, description, **kwargs)
-
-
-def error(code, description=None, **kwargs):
-    """
-    """
-    return Status('error', code, description, **kwargs)
