60c6e8928b2e562b602fa49dacc999a273c549d6
Nick Joyce <nick@boxdesign.co.uk>
2010-10-12 19:58:57 +1000

    Bad merges .. sigh

--- rtmpy/protocol/rtmp/__init__.py
+++ rtmpy/protocol/rtmp/__init__.py
@@ -385,6 +385,10 @@ class RTMPProtocol(protocol.Protocol, BaseStream):
     objectEncoding = pyamf.AMF0
     clientId = None
 
+    def __init__(self):
+        # this protocol is the NetConnection
+        BaseStream.__init__(self, 0)
+
     def logAndDisconnect(self, reason, *args, **kwargs):
         """
         Called when something fatal has occurred. Logs any errors and closes the
@@ -508,9 +512,6 @@ class RTMPProtocol(protocol.Protocol, BaseStream):
         """
         Handshaking was successful, streaming now commences.
         """
-        # this protocol is the NetConnection
-        BaseStream.__init__(self, 0)
-
         self.decoder = codec.Decoder(DecodingDispatcher(self), self)
         self.encoder = codec.Encoder(self.transport)
 
