8ae9d83d5b785cff894a9154505146c2407bb82a
Thijs Triemstra <info@collab.nl>
2010-11-19 19:24:18 +0100

    Update pyamf version requirement ('PyAMF>=0.6' doesnt work) and update get_version() so it works like pyamf's get_version().

--- setup.py
+++ setup.py
@@ -52,38 +52,16 @@ def get_test_requirements():
 
 
 def get_version():
-    """
-    Gets the version number. Pulls it from the source files rather than
-    duplicating it.
-    """
-    import os.path
-    # we read the file instead of importing it as root sometimes does not
-    # have the cwd as part of the PYTHONPATH
+    from rtmpy import __version__
 
-    fn = os.path.join(os.path.dirname(__file__), 'rtmpy', '__init__.py')
-    lines = open(fn, 'rt').readlines()
-
-    version = None
-
-    for l in lines:
-        # include the ' =' as __version__ is a part of __all__
-        if l.startswith('__version__ =', ):
-            x = compile(l, fn, 'single')
-            eval(x)
-            version = locals()['__version__']
-            break
-
-    if version is None:
-        raise RuntimeError('Couldn\'t determine version number')
-
-    return '.'.join([str(x) for x in version])
+    return '.'.join([str(x) for x in __version__])
 
 def get_install_requirements():
     """
     Returns a list of dependencies for RTMPy to function correctly on the
     target platform.
     """
-    install_requires = ['Twisted>=2.5.0', 'PyAMF>=0.6,<=0.6b2']
+    install_requires = ['Twisted>=2.5.0', 'PyAMF>0.5.1']
 
     if sys.platform.startswith('win'):
         install_requires.append('PyWin32')
