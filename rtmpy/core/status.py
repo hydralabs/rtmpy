4ed434c0957a35d2a9d47adaa54d3ebc3858e826
Nick Joyce <nick@boxdesign.co.uk>
2011-01-17 10:35:20 +1000

    Add IStatus and some proper tests.

--- rtmpy/core/status.py
+++ rtmpy/core/status.py
@@ -18,35 +18,83 @@ RTMP status response objects require guaranteed attribute order otherwise the
 Flash Player throws a fit.
 """
 
-
-def status(code, description=None, **kwargs):
-    """
-    A helper method to return a status object with prefilled defaults.
-
-    Use this if you want to return a success status for a given operation.
-    """
-    return Status('status', code, description, **kwargs)
+from zope.interface import Interface, Attribute, implements
 
 
-def error(code, description=None, **kwargs):
+__all__ = ['IStatus', 'status', 'error']
+
+
+STATUS_STATUS = 'status'
+STATUS_ERROR = 'error'
+
+
+class IStatus(Interface):
     """
-    Use this if you want to return a failure status for a given operation.
+    A status object is a generic notification about an operation or state of the
+    endpoint, including error handling.
+
+    Status objects are encoded in AMF and dispatched into (or received from) the
+    RTMP message stream.
+
+    All attributes specified in this interface are the minimum required values
+    for a valid status object.
+
+    All attributes are of type C{unicode} unless explicitly specified.
     """
-    return Status('error', code, description, **kwargs)
+
+    level = Attribute('The level of the notification. Valid values appear to be'
+        ' C{status} and C{error}.')
+    code = Attribute('A label used to distinguish between different types of '
+        'notification. The structure of this string appears to be B{.} '
+        'separated depending on the context of the notification. See '
+        'L{rtmpy.core.status.codes} for a list of known values.')
+    description = Attribute('A humanised description of the notification.')
+
+
+
+def status(code, description, **kwargs):
+    """
+    Returns a L{IStatus} object.
+
+    Use this if you want to return a success notification for a given operation.
+
+    @param code: The code of the status. See L{rtmpy.core.status.codes} for a
+        list of valid values.
+    @param description: A humanised description of the successful operation.
+    @param kwargs: A C{dict} of values that will be added to the status object
+        and will be dispatched to the receiving endpoint.
+    """
+    return Status(STATUS_STATUS, code, description, **kwargs)
+
+
+def error(code, description, **kwargs):
+    """
+    Returns a L{IStatus} in an error state.
+
+    Use this if you want to return a failure notification for a given operation.
+
+    @param code: The code of the status. See L{rtmpy.core.status.codes} for a
+        list of valid values.
+    @param description: A humanised description of the successful operation.
+    @param kwargs: A C{dict} of values that will be added to the status object
+        and will be dispatched to the receiving endpoint.
+    """
+    return Status(STATUS_ERROR, code, description, **kwargs)
+
 
 
 class Status(object):
     """
-    Represents a status response for a given RTMP call or operation.
+    A generic notification object for a given RTMP call or operation.
 
-    @ivar level: level of the status. Valid values appear to be 'status' or
-        'error'.
-    @ivar code: Represents the type of status/error being reported. See
-        L{rtmpy.statuscodes} for a list.
-    @ivar description: A description of the status.
+    @see: L{IStatus} for a more in-depth description.
     """
 
+    implements(IStatus)
+
     class __amf__:
+        # RTMP is very picky about the order of the required attributes.
+        # any others, it doesn't seem to care about
         static = ('level', 'code', 'description')
 
     def __init__(self, level, code, description, **kwargs):
@@ -57,13 +105,15 @@ class Status(object):
         self.__dict__.update(kwargs)
 
     def __repr__(self):
-        return '<%s.%s %r at 0x%x>' % (
+        return '<%s: %s %r at 0x%x>' % (
             self.__class__.__module__,
             self.__class__.__name__,
             self.__dict__,
             id(self)
         )
 
+    def __unicode__(self):
+        return '%s: %s' % (self.code, self.description)
+
     def __eq__(self, other):
         return self.__dict__ == other
-
---
+++ rtmpy/tests/core/test_status.py
@@ -0,0 +1,216 @@
+"""
+Tests for L{rtmpy.core.status}
+"""
+
+from twisted.trial import unittest
+
+from rtmpy.core import status
+
+
+
+class StatusHelperTestCase(unittest.TestCase):
+    """
+    Tests for L{status.status}
+    """
+
+
+    def test_interface(self):
+        """
+        Ensure that the returned status object implements the correct interface.
+        """
+        s = status.status(None, None)
+
+        self.assertTrue(status.IStatus.providedBy(s))
+
+
+    def test_return(self):
+        """
+        Check the attribute allocation.
+        """
+        s = status.status('foo', 'bar', spam='eggs', gak=[1, 2, 3])
+
+        # core attributes
+        self.assertEqual(s.level, 'status')
+        self.assertEqual(s.code, 'foo')
+        self.assertEqual(s.description, 'bar')
+
+        # extra context
+        self.assertEqual(s.spam, 'eggs')
+        self.assertEqual(s.gak, [1, 2, 3])
+
+
+
+class ErrorHelperTestCase(unittest.TestCase):
+    """
+    Tests for L{status.error}
+    """
+
+    def test_interface(self):
+        """
+        Ensure that the returned status object implements the correct interface.
+        """
+        s = status.error(None, None)
+
+        self.assertTrue(status.IStatus.providedBy(s))
+
+
+    def test_return(self):
+        """
+        Check the attribute allocation.
+        """
+        s = status.error('foo', 'bar', spam='eggs', gak=[1, 2, 3])
+
+        # core attributes
+        self.assertEqual(s.level, 'error')
+        self.assertEqual(s.code, 'foo')
+        self.assertEqual(s.description, 'bar')
+
+        # extra context
+        self.assertEqual(s.spam, 'eggs')
+        self.assertEqual(s.gak, [1, 2, 3])
+
+
+
+class StatusTestCase(unittest.TestCase):
+    """
+    Tests for L{status.Status}
+    """
+
+
+    def test_interface(self):
+        """
+        Ensure that the correct interface is implemented.
+        """
+        self.assertTrue(status.IStatus.implementedBy(status.Status))
+
+        s = status.Status(None, None, None)
+
+        self.assertTrue(status.IStatus.providedBy(s))
+
+
+    def test_create(self):
+        """
+        Test attribute allocation.
+        """
+        s = status.Status('foo', 'bar', 'baz', spam='eggs')
+
+        self.assertEqual(s.level, 'foo')
+        self.assertEqual(s.code, 'bar')
+        self.assertEqual(s.description, 'baz')
+
+        self.assertEqual(s.spam, 'eggs')
+
+
+    def test_unicode(self):
+        """
+        Ensure that unicode is handled correctly.
+        """
+        s = status.Status(None, 'bar', 'baz')
+
+        self.assertEqual(unicode(s), 'bar: baz')
+
+    def test_equality(self):
+        """
+        Its all in the name.
+        """
+        s = status.Status('foo', 'bar', 'baz')
+
+        # simple scalar checks
+        self.assertNotEqual(s, 1)
+        self.assertNotEqual(s, None)
+        self.assertNotEqual(s, 'foo')
+
+        ref_dict = {
+            'level': 'foo',
+            'code': 'bar',
+            'description': 'baz'
+        }
+
+        self.assertEqual(s, ref_dict)
+
+
+class AMFEncodingTestCase(unittest.TestCase):
+    """
+    Tests to ensure that AMF encoding works smoothly.
+    """
+
+
+    def setUp(self):
+        import pyamf
+
+        try:
+            pyamf.get_class_alias(status.Status)
+        except pyamf.UnknownClassAlias:
+            self._old_alias = None
+        else:
+            raise RuntimeError(
+                'Unexpected status.Status class registered in PyAMF')
+
+        self.alias = pyamf.register_class(status.Status)
+
+
+    def tearDown(self):
+        import pyamf
+
+        pyamf.unregister_class(status.Status)
+
+        if self._old_alias:
+            pyamf.register_class(self._old_alias)
+
+
+    def test_static_attributes(self):
+        """
+        Ensure that the core attributes will be encoded in order correctly.
+        """
+        self.alias.compile()
+
+        self.assertEqual(
+            self.alias.static_attrs, ['level', 'code', 'description'])
+
+
+    def test_amf0(self):
+        """
+        Test encoding in AMF0.
+        """
+        import pyamf
+
+        ref_dict = {
+            'level': 'alevel',
+            'code': 'Some.Code.Here',
+            'description': 'Look mom, no hands!'
+        }
+
+        s = status.Status(
+            ref_dict['level'],
+            ref_dict['code'],
+            ref_dict['description'])
+
+        blob = pyamf.encode(s, encoding=pyamf.AMF0)
+
+        decoded_status = pyamf.decode(blob, encoding=pyamf.AMF0).next()
+
+        self.assertEqual(decoded_status, s)
+
+
+    def test_amf3(self):
+        """
+        Test encoding in AMF3.
+        """
+        import pyamf
+
+        ref_dict = {
+            'level': 'alevel',
+            'code': 'Some.Code.Here',
+            'description': 'Look mom, no hands!'
+        }
+
+        s = status.Status(
+            ref_dict['level'],
+            ref_dict['code'],
+            ref_dict['description'])
+
+        blob = pyamf.encode(s, encoding=pyamf.AMF3)
+
+        decoded_status = pyamf.decode(blob, encoding=pyamf.AMF3).next()
+
+        self.assertEqual(decoded_status, s)
